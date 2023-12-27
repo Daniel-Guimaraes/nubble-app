54e7cf00ef00a4bd0c832cadbbaf12ff11b847f7
Daniel Vieira <daniel.guimaraes.vieira.dev@gmail.com>
2023-12-27 20:02:59 -0300

    style: create spacing theme

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -39,6 +39,18 @@ export const theme = createTheme({
     success: palette.greenSuccess,
     successLight: palette.greenSuccessLight,
   },
-  spacing: {},
+  spacing: {
+    s4: 4,
+    s8: 8,
+    s12: 12,
+    s16: 16,
+    s24: 24,
+    s32: 32,
+    s40: 40,
+    s48: 48,
+    s56: 56,
+  },
   borderRadii: {},
 });
+
+export type ThemeType = typeof palette;
