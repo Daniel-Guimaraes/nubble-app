4755e3d1e92f95f7a67e3e4ecfd0b680e62d620c
Daniel Vieira <daniel.guimaraes.vieira.dev@gmail.com>
2023-12-28 06:39:37 -0300

    feat: Added default values for edge rounding

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -44,13 +44,18 @@ export const theme = createTheme({
     s8: 8,
     s12: 12,
     s16: 16,
+    s20: 20,
     s24: 24,
     s32: 32,
     s40: 40,
     s48: 48,
     s56: 56,
   },
-  borderRadii: {},
+  borderRadii: {
+    s8: 8,
+    s12: 12,
+    s16: 16,
+  },
 });
 
-export type ThemeType = typeof palette;
+export type ThemeType = typeof theme;
